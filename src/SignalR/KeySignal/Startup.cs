d68d581f158c39a9af410c63595bfca6865a765d
Jef King <jef@agilebusinesscloud.com>
2015-04-18 16:03:13 -0700

    fix project

--- src/SignalR/KeySignal/Startup.cs
+++ src/SignalR/KeySignal/Startup.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Owin;
+﻿using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartup(typeof(KeySignal.Startup))]
@@ -12,7 +9,6 @@ namespace KeySignal
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
         }
     }
 }
