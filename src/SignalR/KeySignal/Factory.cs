133934a90f3936e7fa6a33fe01f79f17cfcc2c3e
Jef King <jef@agilebusinesscloud.com>
2015-04-18 20:29:40 -0700

    more good

---
+++ src/SignalR/KeySignal/Factory.cs
@@ -0,0 +1,14 @@
+﻿using King.Service;
+using System;
+using System.Collections.Generic;
+
+namespace KeySignal
+{
+    public class Factory : ITaskFactory<object>
+    {
+        public IEnumerable<IRunnable> Tasks(object passthrough)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
