c7183473ca7184700a397a21caa809abcd16723d
Jef King <jef@agilebusinesscloud.com>
2015-04-18 19:41:27 -0700

    SignalR transport

--- src/SignalR/KeySignal/Hubs/EchoHub.cs
+++ src/SignalR/KeySignal/Hubs/EchoHub.cs
@@ -1,12 +1,52 @@
 ﻿using Microsoft.AspNet.SignalR;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System.Configuration;
+using System.Threading.Tasks;
 
 namespace KeySignal.Hubs
 {
     public class EchoHub : Hub
     {
+        const string name = "keyspls";
+
+        private readonly string connectionString = ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"];
         public void Send(string character)
         {
             Clients.All.Send(character);
+        }
+
+        public async Task SendStroke(Stroke s)
+        {
+            var eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, name);
+            
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(s);
+            var data = JsonStringToByteArray(json);
+            var msg = new EventData(data);
+            await eventHubClient.SendAsync(msg);
+
+        }
+        public async Task SendExample(Example e)
+        {
+            var eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, name);
+
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(e);
+            var data = JsonStringToByteArray(json);
+            var msg = new EventData(data);
+            await eventHubClient.SendAsync(msg);
+        }
+
+
+        public static byte[] JsonStringToByteArray(string jsonByteString)
+        {
+            jsonByteString = jsonByteString.Substring(1, jsonByteString.Length - 2);
+            string[] arr = jsonByteString.Split(',');
+            byte[] bResult = new byte[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bResult[i] = byte.Parse(arr[i]);
+            }
+            return bResult;
         }
     }
 }
---
+++ src/SignalR/KeySignal/Models.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KeySignal
+{
+    public class Stroke
+    {
+        public DateTime time { get; set; }
+        public int order { get; set; }
+        public int action { get; set; }
+        public string guid { get; set; }
+        public string value { get; set; }
+    }
+
+    public class Example
+    {
+        public string userId { get; set; }
+        public IList<Stroke> strokes { get; set; }
+    }
+}
