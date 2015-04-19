075f18dd0d61f904720290048a3a9e6b319c7cbb
leandroohf <leandroohf@gmail.com>
2015-04-19 02:51:54 -0300

    Update Rlibrary.R
    * Added Latency

--- ml/Rlibrary.R
+++ ml/Rlibrary.R
@@ -1,7 +1,20 @@
+    
   require(rjson)
   require(lubridate)
   
   BuildFakeTextStream <- function(){
+      
+      ## | keystroke | key_down            | key_up              |
+      ## |-----------+---------------------+---------------------|
+      ## | T         | 2015-04-19 01:03:34 | 2015-04-19 01:03:34 |
+      ## | h         | 2015-04-19 01:03:34 | 2015-04-19 01:03:35 |
+      ## | e         | 2015-04-19 01:03:35 | 2015-04-19 01:03:35 |
+      ## |           | 2015-04-19 01:03:36 | 2015-04-19 01:03:36 |
+      ## | t         | 2015-04-19 01:03:37 | 2015-04-19 01:03:37 |
+      ## | e         | 2015-04-19 01:03:37 | 2015-04-19 01:03:37 |
+      ## | a         | 2015-04-19 01:03:38 | 2015-04-19 01:03:38 |
+      ## | m         | 2015-04-19 01:03:38 | 2015-04-19 01:03:39 |
+      ## |-----------+---------------------+---------------------|
   
       text.stream <- data.frame(keystroke=rep("t",8),
                                 key_down=rep("2015-04-19T01:03:34.211Z",8),
@@ -9,7 +22,7 @@
       
       key.down.begin <- as.POSIXct("2015-04-19T01:03:34.213Z",format="%Y-%m-%dT%H:%M:%OSZ")
   
-      key.down.intervals <- c(0.000,0.700,1.300,2.400,2.900,3.400,4.000,4.700)
+      key.down.intervals <- c(0.000,0.700,1.300,2.400,2.900,3.400,4.000,4.713)
       key.up.intervals   <- key.down.intervals + 0.3
   
       key.down <- key.down.begin + dseconds(key.down.intervals)
@@ -28,8 +41,8 @@
   DataMunging <- function(text.stream){
   
       ## Convert to POSIXct in order to use lubridade library
-      text.stream$key_down <- as.POSIXct(text.stream$key_down,format="%Y-%m-%dT%H:%M:%OS")
-      text.stream$key_up <- as.POSIXct(text.stream$key_up,format="%Y-%m-%dT%H:%M:%OS")
+      text.stream$key_down      <- as.POSIXct(text.stream$key_down,format="%Y-%m-%dT%H:%M:%OS")
+      text.stream$key_up        <- as.POSIXct(text.stream$key_up,format="%Y-%m-%dT%H:%M:%OS")
   
       return(text.stream)
   }
@@ -39,16 +52,16 @@
       stopifnot(c("key_down","key_up","keystroke") %in% names(text.stream))
       
   }
-
+  
   ComputeFeatures <-function(text.stream){
   
       text.stream$dwell  <- GetDwell(text.stream)
       text.stream$speed  <- GetSpeed(text.stream)
-      ##text.stream$latency <- GetLatency(text.stream)
+      text.stream$latency <- GetLatency(text.stream)
       
       return(text.stream)
   }
-
+  
   
   GetDwell <- function(text.stream){
   
@@ -60,7 +73,7 @@
   
       number.of.keys <- nrow(text.stream)
   
-      print(number.of.keys)
+      ## print(number.of.keys)
       
       first.keystroke.up.time <- text.stream[1,"key_down"]
   
@@ -70,7 +83,23 @@
                                            end = last.keystroke.up.time))
   
       
-      print(typing.duration)
+      # print(typing.duration)
       speed <- number.of.keys/as.numeric(typing.duration)
       return(speed)
   }
+  
+  GetLatency <- function(text.stream){
+      ## time betwee 2 consecutive up
+  
+      number.of.keys <- nrow(text.stream)
+  
+      start.times <- text.stream$key_up[1:number.of.keys-1]
+      end.times   <- text.stream$key_up[2:number.of.keys]
+      
+      latency <-  dseconds(interval(start= start.times,
+                                    end = end.times))
+  
+      latency <- append(latency,NA)
+      return(latency)
+  }
+  
