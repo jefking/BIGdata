882e50fef095b9ec89138aed8efaf4852f18769e
brunosk2 <bruno.augusto_90@yahoo.com.br>
2015-04-18 17:09:49 -0700

    Update send_data.js

--- send_data.js
+++ send_data.js
@@ -1,5 +1,5 @@
 function sendData() {
- 	var sas = "SharedAccessSignature sr=https%3a%2f%2fkeysaasy.servicebus.windows.net%2fkeyspls%2fpublishers%2fkeypub%2fmessages&sig=cBY8IdXUh%2bjv8o5EQ2lyBtYdXWciLWzPIuIIaPUisLU%3d&se=1429408841&skn=JSSender";
+    var sas = "SharedAccessSignature sr=https%3a%2f%2fkeysaasy.servicebus.windows.net%2fkeyspls%2fpublishers%2fkeypub%2fmessages&sig=cBY8IdXUh%2bjv8o5EQ2lyBtYdXWciLWzPIuIIaPUisLU%3d&se=1429408841&skn=JSSender";
 
     var serviceNamespace = "keysaasy";
     var hubName = "keyspls";
