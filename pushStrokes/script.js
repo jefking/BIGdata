d27f20c3f4ab5aae3a6f9327b44a2c22b3daec2b
jVidal <joao.lvidal@gmail.com>
2015-04-18 21:17:48 -0700

    Changes in push strokes sample project

--- pushStrokes/script.js
+++ pushStrokes/script.js
@@ -22,7 +22,7 @@ $(function () {
         obj.userId = $('#textUser').val();
         obj.strokes = data;
         var value = JSON.stringify(obj);
-        HttpRequest.send(value);
+        //HttpRequest.send(value);
     };
 
     function getTime(arg) {
@@ -30,13 +30,69 @@ $(function () {
     }
 
     var subjectCall = new Rx.Subject();
-    var order;
+    var order = 0;
     var guid;
+    var lastKeyDownTime;
+    var lastKeyDownValues;
 
     $('.KeyStrokeCounter').keydownAsObservable()
         .map(function (arg) {
+            var previousKeyDown = lastKeyDownTime;
+            lastKeyDownTime = getTime(arg);
+
             var time = getTime(arg);
             // beginTime = time;
+            //var action = 3;
+            order = order + 1;
+
+            var data = new Object();
+            data.time = time;
+            data.order = order;
+            //data.action = action;
+            //data.guid = guid;
+            data.value = String.fromCharCode(arg.keyCode);
+
+            if (previousKeyDown !== undefined) {
+                data.interval = lastKeyDownTime.subtract(previousKeyDown);
+            }
+
+            var temp = {
+                time: lastKeyDownTime,
+                key: data.value,
+                interval : data.interval
+            }
+
+            if (lastKeyDownValues === undefined) {
+                lastKeyDownValues = [temp];
+            } else {
+
+                var found = false;
+                for (var i = 0; i < lastKeyDownValues.length; i++) {
+                    if (lastKeyDownValues[i].key === temp.key) {
+                        lastKeyDownValues[i].time = temp.time;
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    lastKeyDownValues.push(temp);
+                    if (lastKeyDownValues.length > 10) {
+                       lastKeyDownValues.splice(0, 1);
+                    }
+                }
+            }
+
+            return data;
+        })
+        //.do(function (arg) {
+            //subjectCall.onNext(arg);
+        //})
+        .subscribe();
+
+    $('.KeyStrokeCounter').keyupAsObservable()
+        .map(function (arg) {
+            var time = getTime(arg);
             var action = 3;
             order = order + 1;
 
@@ -46,24 +102,18 @@ $(function () {
             data.action = action;
             data.guid = guid;
             data.value = String.fromCharCode(arg.keyCode);
-            return data;
-        })
-        .do(function (arg) {
-            subjectCall.onNext(arg);
-        }).subscribe();
 
-    $('.KeyStrokeCounter').keyupAsObservable()
-        .map(function (arg) {
-            var time = getTime(arg);
-            var action = 4;
-            order = order + 1;
+            if (lastKeyDownValues !== undefined) {
 
-            var data = new Object();
-            data.time = time;
-            data.order = order;
-            data.action = action;
-            data.guid = guid;
-            data.value = String.fromCharCode(arg.keyCode);
+                for (var i = 0; i < lastKeyDownValues.length; i++) {
+                    if (lastKeyDownValues[i].key === data.value) {
+                        data.pressinterval = time.subtract(lastKeyDownValues[i].time);
+                        data.interval = lastKeyDownValues[i].interval;
+                        break;
+                    }
+                }
+            }
+            
             return data;
         }).do(function (arg) {
             subjectCall.onNext(arg);
@@ -88,7 +138,6 @@ $(function () {
          guid = genGuid();
          var time = getTime(arg);
          var action = 1;
-         order = 1;
 
          var data = new Object();
          data.time = time;
