b9d4fe39cb5db3040873cfe6693b57201bbd72b8
brunosk2 <bruno.augusto_90@yahoo.com.br>
2015-04-18 17:10:24 -0700

    Update send_data.js

--- send_data.js
+++ send_data.js
@@ -1,10 +1,9 @@
-function sendData() {
+function sendData(Content) {
     var sas = "SharedAccessSignature sr=https%3a%2f%2fkeysaasy.servicebus.windows.net%2fkeyspls%2fpublishers%2fkeypub%2fmessages&sig=cBY8IdXUh%2bjv8o5EQ2lyBtYdXWciLWzPIuIIaPUisLU%3d&se=1429408841&skn=JSSender";
 
     var serviceNamespace = "keysaasy";
     var hubName = "keyspls";
     var deviceName = "keypub";
-    var Content = "Test no xml 2";
  
     var HttpRequest = new XMLHttpRequest();
     HttpRequest.open("POST", "https://" +serviceNamespace + ".servicebus.windows.net/" + hubName + "/publishers/" + deviceName + "/messages", true);
