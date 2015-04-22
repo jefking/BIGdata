eaa6a378de46902f814ced2a96a2579c48c5518e
Jef King <jef@agilebusinesscloud.com>
2015-04-21 18:59:30 -0700

    small code chunk

---
+++ FromHubToSignalR.cs
@@ -0,0 +1,38 @@
+﻿namespace KeySignal
+{
+    using King.Service;
+    using Microsoft.ServiceBus.Messaging;
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web;
+
+    public class FromHubToSignalR : RecurringTask
+    {
+        const string name = "keyspls";
+        private static readonly string connectionString = ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"];
+        private readonly EventHubClient eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, name);
+
+        private readonly string partitionId;
+
+        public FromHubToSignalR(string partitionId)
+        {
+            this.partitionId = partitionId;
+        }
+        public async Task Dequeue()
+        {
+            var group = this.eventHubClient.GetDefaultConsumerGroup();
+            var r = await group.CreateReceiverAsync(partitionId);
+            var data = await r.ReceiveAsync(32);
+            foreach (var d in data)
+            {
+                var json = Convert.ToString(d.GetBytes());
+                var stroke = JsonConvert.DeserializeObject<FlatStroke>(json);
+            }
+        }
+
+    }
+}
